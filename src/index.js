6901cd37d4f968ee382d6094eb020862201f823d
eubrunorossato <Bruno.Araujo1@ibm.com>
2022-02-04 19:52:29 -0300

    login page layout

--- src/index.js
+++ src/index.js
@@ -2,10 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './Router';
+import { ChakraProvider } from '@chakra-ui/react';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <ChakraProvider>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </ChakraProvider>,
   document.getElementById('root')
 );
--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,4 +1,44 @@
+import {
+  Input,
+  Button,
+  InputGroup,
+  Stack,
+  InputLeftElement,
+} from '@chakra-ui/react';
+import { FaUserAlt } from 'react-icons/fa';
+import { VscKey } from 'react-icons/vsc';
+
 import './index.css';
 export default function LoginPage() {
-  return <img className="background" alt="" src="/img/img-bg-login.jpg" />;
+  return (
+    <div className="background">
+      <div className="box">
+        <div className="coco-bambu-logo">
+          <img src="/img/logo-coco-bambu.png" alt="" />
+        </div>
+        <div>
+          <Stack spacing={4}>
+            <InputGroup>
+              <InputLeftElement
+                pointerEvents="none"
+                children={<FaUserAlt color="black" />}
+              />
+              <Input type="tel" placeholder="Nome do usuário" size="lg" />
+            </InputGroup>
+
+            <InputGroup>
+              <InputLeftElement
+                pointerEvents="none"
+                children={<VscKey color="black" />}
+              />
+              <Input placeholder="Senha" size="lg" />
+            </InputGroup>
+          </Stack>
+        </div>
+        <div className="button-div">
+          <Button colorScheme="orange">Acessar</Button>
+        </div>
+      </div>
+    </div>
+  );
 }
