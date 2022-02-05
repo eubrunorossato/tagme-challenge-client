1570e4291585d017fe1a800f038becfa08fbd173
eubrunorossato <Bruno.Araujo1@ibm.com>
2022-02-05 01:06:45 -0300

    login layout

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,41 +1,37 @@
-import {
-  Input,
-  Button,
-  InputGroup,
-  Stack,
-  InputLeftElement,
-} from '@chakra-ui/react';
+import { Input, Button, InputGroup, InputLeftElement } from '@chakra-ui/react';
 import { FaUserAlt } from 'react-icons/fa';
 import { VscKey } from 'react-icons/vsc';
-
+import Tooltip from './tooltip/index';
 import './index.css';
+
 export default function LoginPage() {
   return (
     <div className="background">
       <div className="box">
+        <Tooltip />
         <div className="coco-bambu-logo">
           <img src="/img/logo-coco-bambu.png" alt="" />
         </div>
-        <div>
-          <Stack spacing={4}>
-            <InputGroup>
-              <InputLeftElement
-                pointerEvents="none"
-                children={<FaUserAlt color="black" />}
-              />
-              <Input type="tel" placeholder="Nome do usuário" size="lg" />
-            </InputGroup>
-
-            <InputGroup>
-              <InputLeftElement
-                pointerEvents="none"
-                children={<VscKey color="black" />}
-              />
-              <Input placeholder="Senha" size="lg" />
-            </InputGroup>
-          </Stack>
+        <div className="content-box">
+          <InputGroup>
+            <InputLeftElement
+              pointerEvents="none"
+              children={<FaUserAlt color="black" />}
+            />
+            <Input type="tel" placeholder="Nome do usuário" size="lg" />
+          </InputGroup>
         </div>
-        <div className="button-div">
+
+        <div className="content-box">
+          <InputGroup>
+            <InputLeftElement
+              pointerEvents="none"
+              children={<VscKey color="black" />}
+            />
+            <Input placeholder="Senha" size="lg" />
+          </InputGroup>
+        </div>
+        <div className="content-box">
           <Button colorScheme="orange">Acessar</Button>
         </div>
       </div>
---
+++ src/pages/login/tooltip/index.js
@@ -0,0 +1,13 @@
+import './index.css';
+export default function Tooltip() {
+  return (
+    <div className="main-box">
+      <div className="box-tooltip-content">
+        <div className="new-orders"></div>
+      </div>
+      <div className="box-arrow">
+        <i className="arrow down"></i>
+      </div>
+    </div>
+  );
+}
