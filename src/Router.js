9f6df332f33255ddb9c5158a83e7a0102bc85c91
eubrunorossato <Bruno.Araujo1@ibm.com>
2022-02-07 10:23:59 -0300

    create img on recipe screen

--- src/Router.js
+++ src/Router.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './index.css';
 import LoginPage from './pages/login/index';
 import MainPage from './pages/main/index';
+import RecipePage from './pages/recipe/index';
 
 export default function Router() {
   return (
@@ -10,6 +11,7 @@ export default function Router() {
       <Routes>
         <Route exact path="/login" element={<LoginPage />} />
         <Route exact path="/" element={<MainPage />} />
+        <Route exact path="/recipe" element={<RecipePage />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/pages/recipe/index.js
@@ -0,0 +1,8 @@
+import './index.css';
+export default function Recipe() {
+  return (
+    <div className="recipe-img-box">
+      <img src="/img/prato-bobo-grande.jpg" alt="" />
+    </div>
+  );
+}
