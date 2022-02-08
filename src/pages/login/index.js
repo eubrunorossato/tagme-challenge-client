4c120ad44d6eef0a08b244e64c105928566ce8fa
eubrunorossato <Bruno.Araujo1@ibm.com>
2022-02-07 22:20:48 -0300

    login http call

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,10 +1,34 @@
+import { useState } from 'react';
 import { Input, Button, InputGroup, InputLeftElement } from '@chakra-ui/react';
 import { FaUserAlt } from 'react-icons/fa';
 import { VscKey } from 'react-icons/vsc';
 import Tooltip from './tooltip/index';
+import axios from 'axios';
+import { Buffer } from 'buffer';
 import './index.css';
 
 export default function LoginPage() {
+  function setObj(param, value) {
+    setUserObject({ ...userObj, [param]: value });
+  }
+
+  async function login() {
+    const token = Buffer.from(
+      `${userObj.user}:${userObj.password}`,
+      'utf8'
+    ).toString('base64');
+    const { status, data } = await axios.post(
+      'https://localhost:3000/user-auth',
+      {},
+      {
+        headers: {
+          Authorization: `Basic ${token}`,
+        },
+      }
+    );
+  }
+
+  const [userObj, setUserObject] = useState({ user: '', password: '' });
   return (
     <div className="background">
       <div className="box">
@@ -18,21 +42,35 @@ export default function LoginPage() {
               pointerEvents="none"
               children={<FaUserAlt color="black" />}
             />
-            <Input type="tel" placeholder="Nome do usuário" size="lg" />
+            <Input
+              type="tel"
+              placeholder="Nome do usuário"
+              size="lg"
+              onChange={e => {
+                setObj('user', e.target.value);
+              }}
+            />
           </InputGroup>
         </div>
-
         <div className="content-box">
           <InputGroup>
             <InputLeftElement
               pointerEvents="none"
               children={<VscKey color="black" />}
             />
-            <Input placeholder="Senha" size="lg" />
+            <Input
+              placeholder="Senha"
+              size="lg"
+              onChange={e => {
+                setObj('password', e.target.value);
+              }}
+            />
           </InputGroup>
         </div>
         <div className="content-box">
-          <Button colorScheme="orange">Acessar</Button>
+          <Button colorScheme="orange" onClick={() => login()}>
+            Acessar
+          </Button>
         </div>
       </div>
     </div>
