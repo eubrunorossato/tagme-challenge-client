4a037a241b9430cf72d7ef23a9a11234b3630f27
eubrunorossato <Bruno.Araujo1@ibm.com>
2022-02-10 12:43:40 -0300

    set logout

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -5,34 +5,37 @@ import { GiCook } from 'react-icons/gi';
 import { BsNewspaper } from 'react-icons/bs';
 import { FaUserAlt } from 'react-icons/fa';
 export default function Navbar(props) {
+  function logOut() {
+    localStorage.removeItem('user');
+    window.location.reload();
+  }
+
   return (
-    <>
-      <div className="navbar-box">
-        <div className="logo-box">
-          <img src="/img/logo-coco-bambu-mini.png" alt="" />
+    <div className="navbar-box">
+      <div className="logo-box">
+        <img src="/img/logo-coco-bambu-mini.png" alt="" />
+      </div>
+      <div className="search-box">
+        <InputGroup>
+          <Input
+            placeholder="Buscar Receita"
+            value={props.value}
+            onChange={props.onChange}
+          />
+          <InputRightElement children={<BiSearchAlt color="green.500" />} />
+        </InputGroup>
+      </div>
+      <div className="icon-box">
+        <div>
+          <GiCook />
         </div>
-        <div className="search-box">
-          <InputGroup>
-            <Input
-              placeholder="Buscar Receita"
-              value={props.value}
-              onChange={props.onChange}
-            />
-            <InputRightElement children={<BiSearchAlt color="green.500" />} />
-          </InputGroup>
+        <div>
+          <BsNewspaper />
         </div>
-        <div className="icon-box">
-          <div>
-            <GiCook />
-          </div>
-          <div>
-            <BsNewspaper />
-          </div>
-          <div>
-            <FaUserAlt />
-          </div>
+        <div style={{ cursor: 'Pointer' }}>
+          <FaUserAlt onClick={() => logOut()} />
         </div>
       </div>
-    </>
+    </div>
   );
 }
